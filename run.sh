ee312e7befb6ade165df3911e4ed0f66220967c4
neevparikh <neev_parikh@brown.edu>
2020-04-14 16:21:07 -0400

    Added minimal requirements

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 python train.py \
---env "Pong-v0" \
---model-type cnn \
+--env "CartPole-v1" \
+--model-type mlp \
 --gpu \
 --seed 5 \
---lr 0.00025 \
+--lr 0.0005 \
 --batchsize 32 \
---replay-buffer-size 1000000 \
---warmup-period 50000  \
+--replay-buffer-size 100000 \
+--warmup-period 1000  \
 --episodes 2000 \
 --reward-clip 1 \
 --model-path ./saved_models
