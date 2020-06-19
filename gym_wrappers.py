3f82ed0981b796ce28682d1d797130872bbae5ea
neevparikh <neev_parikh@brown.edu>
2020-06-19 12:01:29 -0400

    Added custom resize_observation due to bug in gym version

--- gym_wrappers.py
+++ gym_wrappers.py
@@ -1,7 +1,28 @@
-import gym
 from collections import deque
+
 import numpy as np
 import torchvision.transforms as T
+import gym
+from gym.spaces import Box
+from gym import ObservationWrapper
+import cv2
+
+# Adapted from https://github.com/openai/gym/blob/master/gym/wrappers/resize_observation.py
+class ResizeObservation(ObservationWrapper):
+    r"""Downsample the image observation to a square image. """
+    def __init__(self, env, shape):
+        super(ResizeObservation, self).__init__(env)
+        if isinstance(shape, int):
+            shape = (shape, shape)
+        assert all(x > 0 for x in shape), shape
+        self.shape = tuple(shape)
+
+        obs_shape = self.shape + self.observation_space.shape[2:]
+        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
+
+    def observation(self, observation):
+        observation = cv2.resize(observation, self.shape[::-1], interpolation=cv2.INTER_AREA)
+        return observation
 
 
 class ObservationDictToInfo(gym.Wrapper):
