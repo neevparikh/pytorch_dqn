1e340a460476e48ea01108fd3a2a97c402fda367
neevparikh <neev_parikh@brown.edu>
2020-06-27 13:27:18 -0400

    Added basic model training

--- abstractions/model_based/model.py
+++ abstractions/model_based/model.py
@@ -1,20 +1,58 @@
 import torch
 
 from ..common.utils import plot_grad_flow
-from ..common.modules import MLP_Body, CNN_Body
 
 class ModelNet(torch.nn.Module):
-    def __init__(self, args, state_space, num_actions):
+    def __init__(self, args, device, state_space, num_actions):
         super(ModelNet, self).__init__()
-        self.body = MLP_Body(state_space, num_actions, args.model_shape)
+        self.state_space = state_space
+        self.num_actions = num_actions
+        self.device = device
+
+        if args.model_shape == 'tiny':
+            self.layer_sizes = [(15,),(15,)]
+        elif args.model_shape == 'small':
+            self.layer_sizes = [(16,), (16, 16), (16, 16), (16, 16), (16,)]
+        elif args.model_shape == 'medium':
+            self.layer_sizes = [(256,), (256, 256), (256, 256), (256, 256), (256,)]
+        elif args.model_shape == 'large':
+            self.layer_sizes = [(1024,), (1024, 1024), (1024, 1024), (1024, 1024), (1024, 1024),
+                    (1024,)]
+        elif args.model_shape == 'giant':
+            self.layer_sizes = [(2048,), (2048, 2048), (2048, 2048), (2048, 2048), (2048, 2048),
+                                (2048, 2048), (2048, 2048), (2048,)]
+
+        self.build_model()
+
+        trainable_parameters = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        print(f"Number of trainable parameters: {trainable_parameters}")
+
         self.optimizer = torch.optim.Adam(self.parameters(), lr=args.lr)
 
-    def forward(self, state, action, reward):
-        pass
 
-    def loss(self, batch, writer=None, writer_step=None):
-        pred_state = self(batch.state, batch.action, batch.reward)
-        loss = torch.nn.functional.mse_loss(pred_state, batch.next_state)
+    def build_model(self):
+        layers = [
+            torch.nn.Linear(self.state_space.shape[0] + 2, self.layer_sizes[0][0]), torch.nn.ReLU(),
+        ]
+        for size in self.layer_sizes[1:-1]:
+            layer = [torch.nn.Linear(size[0], size[1]), torch.nn.ReLU()]
+            layers.extend(layer)
+
+        layers.append(torch.nn.Linear(self.layer_sizes[-1][0], self.state_space.shape[0]))
+
+        self.body = torch.nn.Sequential(*layers)
+
+    def forward(self, state, action, reward):
+        state = torch.as_tensor(state, dtype=torch.float32).to(self.device)
+        action = torch.as_tensor(action, dtype=torch.float32).unsqueeze(-1).to(self.device)
+        reward = torch.as_tensor(reward, dtype=torch.float32).unsqueeze(-1).to(self.device)
+        return self.body(torch.cat([state, action, reward], dim=1))
+
+    def loss(self, state, action, reward, next_state, writer=None, writer_step=None):
+        pred_state = self(state, action, reward)
+        next_state = torch.as_tensor(next_state, dtype=torch.float32).to(self.device)
+
+        loss = torch.nn.functional.mse_loss(pred_state, next_state)
 
         if writer:
             writer.add_scalar('training/step_loss', loss.mean(), writer_step)
@@ -22,18 +60,18 @@ class ModelNet(torch.nn.Module):
         return loss
 
     def train_batch(self, batch, global_steps, writer, gradient_clip):
-        loss = self.loss(batch, writer, global_steps)
+        state, action, reward, next_state, _ = batch
+        loss = self.loss(state, action, reward, next_state, writer, global_steps)
         self.zero_grad()
         loss.backward()
 
         if gradient_clip:
-            torch.nn.utils.clip_grad_norm_(self.online.parameters(), gradient_clip)
+            torch.nn.utils.clip_grad_norm_(self.parameters(), gradient_clip)
 
-        if writer and global_steps % 1000 == 0:
-            writer.add_figure('gradient_flow', plot_grad_flow(self.online.named_parameters()),
+        if writer and global_steps % 1000000 == 0:
+            writer.add_figure('gradient_flow', plot_grad_flow(self.named_parameters()),
                     global_steps)
 
         self.optimizer.step()
 
         return loss.detach().cpu().numpy()
-
--- abstractions/model_based/train.py
+++ abstractions/model_based/train.py
@@ -1,28 +1,77 @@
 import time
 
 import gym
+import torch
+from torch.utils.tensorboard import SummaryWriter
 
 from ..common.replay_buffer import ReplayBuffer
+from ..common.utils import model_based_parser
+from .model import ModelNet
 
-env = gym.make("CartPole-v1")
+def get_action(env):
+    ''' random policy '''
+    return env.action_space.sample()
+    
 
-# Episode loop
-global_steps = 0
-steps = 1
-episode = 0
-start = time.time()
-t_zero = time.time()
-             
-end = time.time() + 1
+if __name__ == "__main__":  
 
-score = 0
-while global_steps < args.max_steps:
-    info_str = "episode: {}, ".format(episode)
-    info_str += "steps: {}, ".format(global_steps)
-    info_str += "ep_score: {}, ".format(score)
-    info_str += "FPS: {}".format(steps/(end - start))
-    print(info_str)
+    args = model_based_parser.parse_args()
+
+    if not args.no_tensorboard:
+        writer = SummaryWriter(comment=args.run_tag)
+    else:
+        writer = None
+
+    # Check if GPU can be used and was asked for
+    if args.gpu and torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+
+    env = gym.make(args.env)
+    replay_buffer = ReplayBuffer(args.replay_buffer_size)
+    
+    model = ModelNet(args, device, env.observation_space, env.action_space.n)
+    model = model.to(device)
+
+    global_steps = 0
+    episode = 0
     start = time.time()
+    end = time.time() + 1
 
-    state = env.reset()
-    done = False
+    while global_steps < args.max_steps:
+        start = time.time()
+
+        state = env.reset()
+        done = False
+
+        cumulative_loss = 0
+        steps = 1
+        while not done:
+            steps += 1
+            action = get_action(env)
+            next_state, reward, done, _ = env.step(action)
+            steps += 1
+            replay_buffer.append(state, action, reward, next_state, int(done))
+
+            global_steps += 1
+
+            if len(replay_buffer) < args.batchsize or global_steps < args.warmup_period:
+                continue
+
+            minibatch = replay_buffer.sample(args.batchsize) 
+            loss = model.train_batch(minibatch, global_steps, writer, args.gradient_clip)
+            cumulative_loss += loss
+
+        end = time.time()
+
+        if writer and global_steps >= args.warmup_period and len(replay_buffer) >= args.batchsize:
+            writer.add_scalar('training/episode_loss', cumulative_loss, episode)
+
+        info_str = "episode: {}, ".format(episode)
+        info_str += "steps: {}, ".format(global_steps)
+        info_str += "loss: {}, ".format(cumulative_loss)
+        info_str += "FPS: {}".format(steps/(end - start))
+        print(info_str)
+
+        episode += 1
