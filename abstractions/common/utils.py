e905557ffee24472ed2ddcf1597588be9e341d8c
neevparikh <neev_parikh@brown.edu>
2020-07-09 12:58:51 -0400

    Added SAC utils

--- abstractions/common/utils.py
+++ abstractions/common/utils.py
@@ -253,9 +253,39 @@ def initialize_environment(args):
         env = make_ari(env)
         test_env = make_ari(test_env)
 
-    if type(env.action_space) != gym.spaces.Discrete:
-        raise NotImplementedError("DQN for continuous action_spaces hasn't been\
-                implemented"                                                        )
     env.seed(args.seed)
     test_env.seed(args.seed + 1000)
     return env, test_env
+
+## SAC utils ##
+
+def create_log_gaussian(mean, log_std, t):
+    quadratic = -((0.5 * (t - mean) / (log_std.exp())).pow(2))
+    l = mean.shape
+    log_z = log_std
+    z = l[-1] * math.log(2 * math.pi)
+    log_p = quadratic.sum(dim=-1) - log_z.sum(dim=-1) - 0.5 * z
+    return log_p
+
+def logsumexp(inputs, dim=None, keepdim=False):
+    if dim is None:
+        inputs = inputs.view(-1)
+        dim = 0
+    s, _ = torch.max(inputs, dim=dim, keepdim=True)
+    outputs = s + (inputs - s).exp().sum(dim=dim, keepdim=True).log()
+    if not keepdim:
+        outputs = outputs.squeeze(dim)
+    return outputs
+
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+def weights_init_(m):
+    if isinstance(m, torch.nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight, gain=1)
+        torch.nn.init.constant_(m.bias, 0)
