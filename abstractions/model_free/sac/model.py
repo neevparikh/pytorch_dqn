a9ca944a5069ae7edd74895abe51a6d0b9b09e35
neevparikh <neev_parikh@brown.edu>
2020-07-09 13:01:21 -0400

    Added SAC model and train file

---
+++ abstractions/model_free/sac/model.py
@@ -0,0 +1,145 @@
+import os
+
+import torch
+import numpy as np
+
+from ...common.modules import GaussianPolicy, QNetwork, DeterministicPolicy
+from ...common.utils import hard_update, soft_update
+from ...common.replay_buffer import Experience
+
+
+class SAC:
+    def __init__(self, num_inputs, action_space, device, args):
+
+        self.gamma = args.gamma
+        self.tau = args.target_moving_average
+        self.alpha = args.alpha
+
+        self.policy_type = args.policy
+        self.target_update_interval = args.target_update_interval
+        self.automatic_entropy_tuning = args.automatic_entropy_tuning
+
+        self.device = device
+
+        self.critic = QNetwork(num_inputs, action_space.shape[0],
+                               args.hidden_size).to(device=self.device)
+        self.critic_optim = torch.optim.Adam(self.critic.parameters(), lr=args.lr)
+
+        self.critic_target = QNetwork(num_inputs, action_space.shape[0],
+                                      args.hidden_size).to(self.device)
+        hard_update(self.critic_target, self.critic)
+
+        if self.policy_type == "Gaussian":
+            # Target Entropy = −dim(A) (e.g. , -6 for HalfCheetah-v2) as given in the paper
+            if self.automatic_entropy_tuning is True:
+                self.target_entropy = -torch.prod(torch.Tensor(action_space.shape).to(
+                    self.device)).item()
+                self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
+                self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=args.lr)
+
+            self.policy = GaussianPolicy(num_inputs,
+                                         action_space.shape[0],
+                                         args.hidden_size,
+                                         action_space).to(self.device)
+            self.policy_optim = torch.optim.Adam(self.policy.parameters(), lr=args.lr)
+
+        else:
+            self.alpha = 0
+            self.automatic_entropy_tuning = False
+            self.policy = DeterministicPolicy(num_inputs,
+                                              action_space.shape[0],
+                                              args.hidden_size,
+                                              action_space).to(self.device)
+            self.policy_optim = torch.optim.Adam(self.policy.parameters(), lr=args.lr)
+
+    def select_action(self, state, evaluate=False):
+        state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
+        if evaluate is False:
+            action, _, _ = self.policy.sample(state)
+        else:
+            _, _, action = self.policy.sample(state)
+        return action.detach().cpu().numpy()[0]
+
+    def update_parameters(self, memory, batch_size, updates):
+        # Sample a batch from memory
+        minibatch = memory.sample(batch_size)
+        minibatch = Experience(*minibatch)
+
+        state_batch = torch.as_tensor(minibatch.state.astype(np.float32)).to(self.device)
+        next_state_batch = torch.as_tensor(minibatch.next_state.astype(np.float32)).to(self.device)
+        action_batch = torch.FloatTensor(minibatch.action).to(self.device, dtype=torch.float32)
+        reward_batch = torch.FloatTensor(minibatch.reward).to(self.device, dtype=torch.float32)
+        reward_batch = reward_batch.unsqueeze(-1)
+        mask_batch = torch.FloatTensor(minibatch.done).to(self.device).unsqueeze(1)
+
+        with torch.no_grad():
+            next_state_action, next_state_log_pi, _ = self.policy.sample(next_state_batch)
+            qf1_next_target, qf2_next_target = self.critic_target(next_state_batch,
+                    next_state_action)
+            min_qf_next_target = torch.min(qf1_next_target,
+                                           qf2_next_target) - self.alpha * next_state_log_pi
+            next_q_value = reward_batch + mask_batch * self.gamma * (min_qf_next_target)
+
+        # Two Q-functions to mitigate positive bias in the policy improvement step
+        qf1, qf2 = self.critic(state_batch, action_batch)
+
+        # JQ = 𝔼(st,at)~D[0.5(Q1(st,at) - r(st,at) - γ(𝔼st+1~p[V(st+1)]))^2]
+        qf1_loss = torch.nn.functional.mse_loss(qf1, next_q_value)
+        qf2_loss = torch.nn.functional.mse_loss(qf2, next_q_value)
+
+        qf_loss = qf1_loss + qf2_loss
+
+        self.critic_optim.zero_grad()
+        qf_loss.backward()
+        self.critic_optim.step()
+
+        pi, log_pi, _ = self.policy.sample(state_batch)
+
+        qf1_pi, qf2_pi = self.critic(state_batch, pi)
+        min_qf_pi = torch.min(qf1_pi, qf2_pi)
+
+        # Jπ = 𝔼st∼D,εt∼N[α * logπ(f(εt;st)|st) − Q(st,f(εt;st))]
+        policy_loss = ((self.alpha * log_pi) - min_qf_pi).mean()
+
+        self.policy_optim.zero_grad()
+        policy_loss.backward()
+        self.policy_optim.step()
+
+        if self.automatic_entropy_tuning:
+            alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
+
+            self.alpha_optim.zero_grad()
+            alpha_loss.backward()
+            self.alpha_optim.step()
+
+            self.alpha = self.log_alpha.exp()
+            alpha_tlogs = self.alpha.clone()  # For TensorboardX logs
+        else:
+            alpha_loss = torch.tensor(0.).to(self.device)
+            alpha_tlogs = torch.tensor(self.alpha)  # For TensorboardX logs
+
+        if updates % self.target_update_interval == 0:
+            soft_update(self.critic_target, self.critic, self.tau)
+
+        return qf1_loss.item(), qf2_loss.item(), policy_loss.item(), alpha_loss.item(), alpha_tlogs.item()
+
+    # Save model parameters
+    def save_model(self, env_name, suffix="", actor_path=None, critic_path=None):
+        if not os.path.exists('models/'):
+            os.makedirs('models/')
+
+        if actor_path is None:
+            actor_path = "models/sac_actor_{}_{}".format(env_name, suffix)
+        if critic_path is None:
+            critic_path = "models/sac_critic_{}_{}".format(env_name, suffix)
+        print('Saving models to {} and {}'.format(actor_path, critic_path))
+        torch.save(self.policy.state_dict(), actor_path)
+        torch.save(self.critic.state_dict(), critic_path)
+
+    # Load model parameters
+    def load_model(self, actor_path, critic_path):
+        print('Loading models from {} and {}'.format(actor_path, critic_path))
+        if actor_path is not None:
+            self.policy.load_state_dict(torch.load(actor_path))
+        if critic_path is not None:
+            self.critic.load_state_dict(torch.load(critic_path))
---
+++ abstractions/model_free/sac/train.py
@@ -0,0 +1,180 @@
+# Adapted from https://github.com/pranz24/pytorch-soft-actor-critic (2020)
+import time
+import os
+import json
+
+import numpy as np
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from .model import SAC
+from ...common.replay_buffer import ReplayBuffer
+from ...common.utils import sac_parser, initialize_environment, reset_seeds, append_timestamp
+
+
+def test_policy(test_env, agent, episode, global_steps, writer, log_filename, args):
+    with torch.no_grad():
+        # Reset environment
+        cumulative_reward = 0
+
+        for _ in range(args.episodes_per_eval):
+            test_state = test_env.reset()
+
+            test_done = False
+            render = args.render and (episode % args.render_episodes == 0)
+
+            # Test episode loop
+            while not test_done:
+                test_action = agent.select_action(test_state, evaluate=True)
+
+                # Take action in env
+                if render:
+                    test_env.render()
+
+                test_state, test_reward, test_done, _ = test_env.step(test_action)
+
+                # Update reward
+                cumulative_reward += test_reward
+
+        eval_reward = cumulative_reward/args.episodes_per_eval
+
+        print("Policy_reward for test:", eval_reward)
+
+        # Logging
+        if not args.no_tensorboard:
+            writer.add_scalar('validation/policy_reward', eval_reward, global_steps)
+        if log_filename:
+            with open(log_filename, "a") as f:
+                f.write("{},{},{},".format(episode, global_steps, eval_reward))
+
+
+def episode_loop(env, test_env, agent, replay_buffer, args, writer):
+    # Episode loop
+    global_steps = 0
+    steps = 1
+    episode = 0
+    updates = 0
+    start = time.time()
+    t_zero = time.time()
+                 
+    end = time.time() + 1
+
+    score = 0
+    while global_steps < args.max_steps:
+        info_str = "episode: {}, ".format(episode)
+        info_str += "steps: {}, ".format(global_steps)
+        info_str += "ep_score: {}, ".format(score)
+        info_str += "FPS: {}".format(steps/(end - start))
+        print(info_str)
+        start = time.time()
+
+        state = env.reset()
+        done = False
+
+        steps = 1
+        score = 0
+
+        # Collect data from the environment
+        while not done:
+            can_train = len(replay_buffer) >= args.batchsize and global_steps > args.warmup_period
+            if can_train:
+                action = env.action_space.sample()
+            else:
+                action = agent.select_action(state)
+            
+            if can_train:
+                for _ in range(args.updates_per_step):
+                    # Update parameters of all the networks
+                    result = agent.update_parameters(replay_buffer, args.batchsize, updates)
+                    critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = result
+
+                    if writer:
+                        writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+                        writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+                        writer.add_scalar('loss/policy', policy_loss, updates)
+                        writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+                        writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+                    updates += 1
+            
+
+            next_state, reward, done, _ = env.step(action)
+            score += reward
+            steps += 1
+            if args.reward_clip:
+                clipped_reward = np.clip(reward, -args.reward_clip, args.reward_clip)
+            else:
+                clipped_reward = reward
+
+            mask = 1 if steps == env._max_episode_steps else not done # pylint: disable=protected-access
+
+            # Store in replay buffer
+            replay_buffer.append(state, action, clipped_reward, next_state, int(mask))
+            state = next_state
+
+            # Testing policy
+            if global_steps % args.test_policy_steps == 0:
+                test_policy(test_env, agent, episode, global_steps, writer, log_filename, args)
+                if log_filename:
+                    with open(log_filename, "a") as f:
+                        f.write("{:.2f}\n".format(time.time() - t_zero))
+
+            global_steps += 1
+
+        end = time.time()
+
+        episode += 1
+
+
+args = sac_parser.parse_args()
+
+# Set seeds
+reset_seeds(args.seed)
+
+# Initialize envs
+env, test_env = initialize_environment(args)
+
+# Check if GPU can be used and was asked for
+if args.gpu and torch.cuda.is_available():
+    device = torch.device('cuda:0')
+else:
+    device = torch.device('cpu')
+
+# Initialize model
+agent = SAC(env.observation_space.shape[0], env.action_space, device, args)
+
+# Save path
+if args.model_path:
+    os.makedirs(args.model_path, exist_ok=True)
+
+if args.output_path:
+    os.makedirs(args.output_path, exist_ok=True)
+
+# Logging via csv
+if args.output_path:
+    base_filename = os.path.join(args.output_path, args.run_tag)
+    os.makedirs(base_filename, exist_ok=True)
+    log_filename = os.path.join(base_filename, 'reward.csv')
+    with open(log_filename, "w") as f:
+        f.write("episode,steps,reward,runtime\n")
+    with open(os.path.join(base_filename, 'params.json'), 'w') as fp:
+        param_dict = vars(args).copy()
+        del param_dict['output_path']
+        del param_dict['model_path']
+        json.dump(param_dict, fp)
+else:
+    log_filename = None
+
+# Logging for tensorboard
+if not args.no_tensorboard:
+    writer = SummaryWriter(comment=args.run_tag)
+else:
+    writer = None
+
+replay_buffer = ReplayBuffer(args.replay_buffer_size)
+episode_loop(env, test_env, agent, replay_buffer, args, writer)
+
+env.close()
+test_env.close()
+
+if args.model_path:
+    torch.save(agent.online, append_timestamp(os.path.join(args.model_path, args.run_tag)) + ".pth")
